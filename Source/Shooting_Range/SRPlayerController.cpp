7c1ddb5ebe5ed8fba95474dd6a82a168e43a685d
highstar97 <pgt0185@gmail.com>
2022-10-11 14:00:31 +0900

    [추가] HuddyWidget(조준점) 장착

--- Source/Shooting_Range/SRPlayerController.cpp
+++ Source/Shooting_Range/SRPlayerController.cpp
@@ -1,13 +1,29 @@
 #include "SRPlayerController.h"
+#include "Blueprint/UserWidget.h"
+
+ASRPlayerController::ASRPlayerController()
+{
+	static ConstructorHelpers::FClassFinder<UUserWidget> UI_HUDDY_C(TEXT("/Game/Blueprints/Huddy_BP.Huddy_BP_C"));
+	if (UI_HUDDY_C.Succeeded())
+	{
+		HuddyWidgetClass = UI_HUDDY_C.Class;
+	}
+}
+
+void ASRPlayerController::BeginPlay()
+{
+	HuddyWidget = CreateWidget<UUserWidget>(this, HuddyWidgetClass);
+	HuddyWidget->AddToViewport();
+}
 
 void ASRPlayerController::PostInitializeComponents()
 {
 	Super::PostInitializeComponents();
-	UE_LOG(LogTemp, Log, TEXT("Player Controller PostInitializeComponetns"));
+	UE_LOG(LogTemp, Warning, TEXT("Player Controller PostInitializeComponents"));
 }
 
 void ASRPlayerController::OnPossess(APawn* aPawn)
 {
-	UE_LOG(LogTemp, Log, TEXT("Player Controller OnPossess"));
+	UE_LOG(LogTemp, Warning, TEXT("Player Controller OnPossess"));
 	Super::OnPossess(aPawn);
 }
--- Source/Shooting_Range/SRPlayerController.h
+++ Source/Shooting_Range/SRPlayerController.h
@@ -7,8 +7,19 @@ UCLASS()
 class SHOOTING_RANGE_API ASRPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
+
 public:
+	ASRPlayerController();
+
+	virtual void BeginPlay() override;
 	virtual void PostInitializeComponents() override;
 	virtual void OnPossess(APawn* aPawn) override;
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = UI)
+	TSubclassOf<UUserWidget> HuddyWidgetClass;
+
+private:
+	UPROPERTY()
+	UUserWidget* HuddyWidget;
 };
